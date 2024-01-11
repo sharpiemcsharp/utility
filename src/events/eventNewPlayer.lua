132f097dbee01150ac7c3c31e71509b5a67cd873
Sharpie McSharp <github.com/sharpiemcsharp>
2024-01-11 12:07:00 +0000

    added tribeName to player object

--- src/events/eventNewPlayer.lua
+++ src/events/eventNewPlayer.lua
@@ -1,72 +1,88 @@
 function eventNewPlayer(name)
-    local player={
-        activeSegments={},
-        name=name,
-        lang=tfm.get.room.playerList[name].community or "en"
+    local player = {
+        activeSegments = {},
+        name = name,
+        lang = tfm.get.room.playerList[name].community or "en",
+        tribeName = tfm.get.room.playerList[name].tribeName,
     }
     if not ranks[name] then
-        ranks[name]=1
+        ranks[name] = 1
     end
     -- Combine defaultPlayer with player
-    for _,s in pairs(_S) do
+    for _, s in pairs(_S) do
         if s.defaultPlayer then
             s.defaultPlayer(player)
         end
     end
-    players[name]=player
+    players[name] = player
     tfm.exec.lowerSyncDelay(name)
 
     -- Activates segments
-    for sn,s in pairs(_S) do
+    for sn, s in pairs(_S) do
         if player.activeSegments[sn] then
-            activateSegment(name,sn)
+            activateSegment(name, sn)
         end
     end
 
     -- Show recent changelog
-    if showChangelog(7,3,player) then
-        tfm.exec.chatMessage(showChangelog(7,3,player),name)
+    if showChangelog(7, 3, player) then
+        tfm.exec.chatMessage(showChangelog(7, 3, player), name)
     end
 
     -- Show random greeting message
-    local greets=translate("greetings",player.lang)
-    tfm.exec.chatMessage("<J>"..greets[math.random(#greets)],name)
+    local greets = translate("greetings", player.lang)
+    tfm.exec.chatMessage("<J>" .. greets[math.random(#greets)], name)
 
-    if (shouldBeAdmin(player)) then
+    if shouldBeAdmin(player) then
         ranks[name] = 4
     end
 
     if not ranks[name] then
-        ranks[name]=1
+        ranks[name] = 1
     end
 
-    if ranks[name]>=RANKS.ROOM_ADMIN then
-        for n,r in pairs(ranks) do
-            if r>=RANKS.ROOM_ADMIN and players[n]then
-                tfm.exec.chatMessage("<font color='#AAAAAA'>&#926; ["..upper(moduleName).."] "..(translate("joinedroom",players[n].lang):format(name)).."</font>",n)
-           end
-       end
+    if ranks[name] >= RANKS.ROOM_ADMIN then
+        for n, r in pairs(ranks) do
+            if r >= RANKS.ROOM_ADMIN and players[n] then
+                tfm.exec.chatMessage(
+                    "<font color='#AAAAAA'>&#926; ["
+                        .. upper(moduleName)
+                        .. "] "
+                        .. (translate("joinedroom", players[n].lang):format(name))
+                        .. "</font>",
+                    n
+                )
+            end
+        end
     end
 
-    system.bindMouse(name,true)
+    system.bindMouse(name, true)
 
     -- Notify listeners
-    notifyListeners(function(sn,s)
+    notifyListeners(function(sn, s)
         if not s.disabled or (map.segments and map.segments[sn]) then
-            local cb=s.callbacks.newPlayer
+            local cb = s.callbacks.newPlayer
             if cb then
                 cb(player)
             end
         end
     end)
     if SETTINGS.QUICKRESPAWN then
-        toRespawn[name]=os.time()
+        toRespawn[name] = os.time()
     end
     if _S.global.tempMapName then
-        ui.setMapName("<J>".._S.global.tempMapName)
+        ui.setMapName("<J>" .. _S.global.tempMapName)
     else
         setMapName()
     end
-    if map.bg then for i,image in pairs(map.bg) do tfm.exec.addImage(image.img,"?"..(1-i),image.x,image.y) end end
-    if map.fg then for i,image in pairs(map.fg) do tfm.exec.addImage(image.img,"!"..(50+i),image.x,image.y) end end
+    if map.bg then
+        for i, image in pairs(map.bg) do
+            tfm.exec.addImage(image.img, "?" .. (1 - i), image.x, image.y)
+        end
+    end
+    if map.fg then
+        for i, image in pairs(map.fg) do
+            tfm.exec.addImage(image.img, "!" .. (50 + i), image.x, image.y)
+        end
+    end
 end
