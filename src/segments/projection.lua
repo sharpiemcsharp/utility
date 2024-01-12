1a8d972da651ae1b821e111646667c406d9e688c
Sharpie McSharp <github.com/sharpiemcsharp>
2024-01-12 01:53:19 +0000

    whitespace tidy

--- src/segments/projection.lua
+++ src/segments/projection.lua
@@ -3,7 +3,7 @@
 _S.projection = {
     callbacks={
         keyboard={
-            [KEYS.LEFT]=function(player,down,x,y) 
+            [KEYS.LEFT]=function(player,down,x,y)
                 if down then
                     if player.dash and player.dash.direction=="left" and player.dash.time>os.time()-250 and (player.lastDash and player.lastDash<os.time()-3000 or not player.lastDash) then
                         tfm.exec.movePlayer(player.name, x-100, y)
@@ -16,7 +16,7 @@ _S.projection = {
                     player.dash={time=os.time(),direction="left"}
                 end
             end,
-            [KEYS.RIGHT]=function(player,down,x,y) 
+            [KEYS.RIGHT]=function(player,down,x,y)
                 if down then
                     if player.dash and player.dash.direction=="right" and player.dash.time>os.time()-250 and (player.lastDash and player.lastDash<os.time()-3000 or not player.lastDash) then
                         tfm.exec.movePlayer(player.name, x+100, y)
