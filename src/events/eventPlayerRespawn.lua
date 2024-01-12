acccfcff97409d0a6757bb5d7297f18e198195e9
Sharpie McSharp <github.com/sharpiemcsharp>
2024-01-12 01:45:18 +0000

    cache table lookup

--- src/events/eventPlayerRespawn.lua
+++ src/events/eventPlayerRespawn.lua
@@ -1,11 +1,14 @@
 function eventPlayerRespawn(name)
-    players[name].facingRight=true
-    players[name].lastSpawn=os.time()
-    -- Notify listeners
-    notifyNameListeners(name, function(player,sn,s)
-        local cb=s.callbacks.playerRespawn
-        if cb then
-            cb(player)
-        end
-    end)
+    local p = players[name]
+    if p then
+        p.facingRight = true
+        p.lastSpawn   = os.time()
+        -- Notify listeners
+        notifyNameListeners(name, function(player, sn, s)
+            local cb = s.callbacks.playerRespawn
+            if cb then
+                cb(player)
+            end
+        end)
+    end
 end
